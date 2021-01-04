04be5a79377fb552456ecbe83498ca1eca161d68
bruno8a10 <bruno8a108a@gmail.com>
2021-01-04 13:29:03 -0300

    Primer commit de 1 al 8

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,29 +1,44 @@
 // Do not change any of the function names
-
 function invocarCallback(cb) {
   // Invoca al callback `cb`
+  cb();
 }
 
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
+   var suma = 0;
+  for (i = 0; i < numeros.length; i++){
+  suma = suma + numeros[i];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  for (i = 0; i < array.length; i++){
+    cb(array[i]);
+    }
 }
-
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
+  //nuevoarray =[];
+  //var nuevoArray = [];
+  //for(i = 0; i< array.length;i++){
+  //nuevoArray.push(cb(array[i]));
+  //nuevoArray[i] = cb(array[i]);
+  //}
+  var nuevoArray = array.map(function(array) {
+    return cb(array);
+  });
+  return nuevoArray;
 }
-
 // No modificar nada debajo de esta línea
 // --------------------------------
-
 module.exports = {
   invocarCallback,
   sumarArray,
